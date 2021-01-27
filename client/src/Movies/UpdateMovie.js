9a7dbbe557c99dbed59cbd72eb52d1c53d47763d
Shazeen Fabius <shazeenfabius@Shazeens-Mini.lan>
2021-01-27 13:10:58 -0800

    setting up update form

---
+++ client/src/Movies/UpdateMovie.js
@@ -0,0 +1,19 @@
+import React from "react";
+/*
+    title: "The Godfather",
+    director: "Francis Ford Coppola",
+    metascore: 100,
+    stars: ["Marlon Brando", "Al Pacino", "Robert Duvall"]
+*/
+
+export default function UpdtaeMovie() {
+  return (
+    <React.Fragment>
+      <form>
+        <label htmlFor="title">
+          <input type="text" />
+        </label>
+      </form>
+    </React.Fragment>
+  );
+}
